2a58f1850b61513e1ded55a1c8395c8e8c62f28d
Олег Гавришків <olehtvink@gmail.com>
2024-10-04 18:23:37 +0300

    Оновлення 04.10.2024

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const auth = document.getElementById('auth')
-const main_page = document.getElementById('main-page')
-const anonim_forum = document.getElementById('anonim_forum')
+const auth = document.getElementById('auth');
+const main_page = document.getElementById('main-page');
+const anonim_forum = document.getElementById('anonim_forum');
+const my_diary = document.getElementById('my_diary');
 
 auth.style.display = 'block';
 
@@ -11,4 +12,8 @@ function authFunc() {
 function afFunc() {
     anonim_forum.style.display = 'block';
     main_page.style.display = 'none';
+}
+function diaryFunc() {
+    main_page.style.display = 'none';
+    my_diary.style.display = 'block';
 }
